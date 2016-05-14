2c12436e5441619f6cab9eac97a6d89ad7c55a50
Jian <ferdinand31@sohu.com>
2016-05-14 14:02:11 +0800

    增加PickerBase

---
+++ CJUWPExtension.Common.UI/Picker/PickerBase.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using CJUWPExtension.Common.UI.Session;
+
+namespace CJUWPExtension.Common.UI.Picker
+{
+
+    public abstract class PickerBase : Control
+    {
+
+        protected abstract PickerPageNavigationInfo PickerPageInfo();
+
+        internal ApplicationSessionPage SessionPage { get; set; }
+        
+        public void Open()
+        {
+            WillOpen();
+            var pickerPageInfo = PickerPageInfo();
+            pickerPageInfo.Picker = this;
+            (Window.Current.Content as Frame)?.Navigate(typeof(PickerRootPage), pickerPageInfo, pickerPageInfo.NavigationTransitionInfo);
+        }
+
+        protected virtual void WillOpen()
+        {
+            
+        }
+
+        public void Close()
+        {
+            WillClose();
+            SessionPage.TryCloseSession();
+        }
+
+        protected virtual void WillClose()
+        {
+
+        }
+
+    }
+
+}
---
+++ CJUWPExtension.Common.UI/Picker/PickerRootPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Windows.UI.Xaml.Media.Animation;
+using CJUWPExtension.Common.UI.Session;
+
+namespace CJUWPExtension.Common.UI.Picker
+{
+
+    public class PickerPageNavigationInfo
+    {
+
+        public Type PickerPageType { get; set; }
+
+        public object PickerPageParameter { get; set; }
+
+        public NavigationTransitionInfo NavigationTransitionInfo { get; set; }
+
+        internal PickerBase Picker { get; set; }
+
+    }
+
+    public sealed partial class PickerRootPage
+    {
+
+        public PickerPageNavigationInfo PickerPageNavigationInfo { get; private set; }
+
+        public PickerRootPage()
+        {
+            InitializeComponent();
+        }
+
+        public override ApplicationSessionFrame RootFrame => RootFrameInstance;
+
+        protected override void DoNavigationToRootPage(object parameter)
+        {
+            PickerPageNavigationInfo = parameter as PickerPageNavigationInfo;
+            if (PickerPageNavigationInfo?.Picker != null)
+            {
+                PickerPageNavigationInfo.Picker.SessionPage = this;
+            }
+            RootFrame.Navigate(PickerPageNavigationInfo?.PickerPageType, this, PickerPageNavigationInfo?.NavigationTransitionInfo);
+        }
+
+    }
+
+}
