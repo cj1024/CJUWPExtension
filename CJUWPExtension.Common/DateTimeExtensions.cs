40b91d95128cc58e8020acca75b03a2dfd46569d
Jian <ferdinand31@sohu.com>
2016-05-17 22:57:54 +0800

    Add Singleton And DateTime Utility

---
+++ CJUWPExtension.Common/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CJUWPExtension.Common
+{
+
+    public static class DateTimeExtensions
+    {
+
+        private static readonly DateTime UtcStandardTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static double UnixTimeStamp(this DateTime time)
+        {
+            return (time.ToUniversalTime() - UtcStandardTime).TotalSeconds;
+        }
+
+    }
+
+}
---
+++ CJUWPExtension.Common/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CJUWPExtension.Common
+{
+
+    public abstract class Singleton<T>
+        where T : new ()
+    {
+
+        private static readonly Lazy<T> _sharedInstance = new Lazy<T>();
+
+        public static T SharedInstance => _sharedInstance.Value;
+
+    }
+
+}
